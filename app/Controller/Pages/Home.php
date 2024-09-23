963e0e3a9ee45ac755d8dabe5d1b50752a34a5ea
zoritto <zoritto@zoritto.zoritto>
2024-09-23 07:55:03 -0300

    feat: adicionando o gerenciamento
    finalizando o models junto com as entidades
    
    finalizando o gerenciamento

--- app/Controller/Pages/Home.php
+++ app/Controller/Pages/Home.php
@@ -3,16 +3,19 @@
 namespace App\Controller\Pages;
 
 use \App\Utils\View;
+use App\Model\Entity\Organization;
 
 class Home extends Page
 {
     public static function getHome()
     {
-       $content = View::render('pages/home', [
-            'name' => 'WDEV - canal',
-            'description' => 'Canal do youtube: https://www.youtube.com/watch?v=30upeqFWLRM',
-            'site' => 'https://github.com/ZorittoWendy'
-       ]);
-       return parent::getPage('Zoritto - Canal - HOME', $content);
+        $obOrganization = new Organization;
+
+        $content = View::render('pages/home', [
+            'name' => $obOrganization->name,
+            'description' => $obOrganization->description,
+            'site' => $obOrganization->site
+        ]);
+        return parent::getPage('Zoritto - Canal - HOME', $content);
     }
 }
---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Model\Entity;
+
+
+
+class Organization
+{
+
+    public $id = 1;
+
+    public $name = 'Canal zorittoDEV';
+
+    public $site = 'https://www.youtube.com/watch?v=30upeqFWLRM';
+
+    public $description = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci temporibus quisquam rerum nisi dolorum delectus neque quia ratione recusandae aut quaerat officia impedit, voluptatum quidem accusamus ut eligendi nemo veritatis?';
+}
